dc665879cfe011c09b4ff51ba2c4b3369464c700
Sten <stenio1212@gmail.com>
2025-09-29 17:03:33 -0400

    added actual file to binary-search-tree file

--- binary-search-tree-solution/binary-search-tree.js
+++ binary-search-tree-solution/binary-search-tree.js
@@ -11,83 +11,118 @@ class BinarySearchTree {
     this.root = root;
   }
 
-  /** insert(val): insert a new node into the BST with value val.
-   * Returns the tree. Uses iteration. */
-
   insert(val) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
 
+    let current = this.root;
+    while (true) {
+      if (val < current.val) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
   }
 
-  /** insertRecursively(val): insert a new node into the BST with value val.
-   * Returns the tree. Uses recursion. */
+  insertRecursively(val, current = this.root) {
+    if (!this.root) {
+      this.root = new Node(val);
+      return this;
+    }
 
-  insertRecursively(val) {
-
+    if (val < current.val) {
+      if (!current.left) {
+        current.left = new Node(val);
+      } else {
+        this.insertRecursively(val, current.left);
+      }
+    } else {
+      if (!current.right) {
+        current.right = new Node(val);
+      } else {
+        this.insertRecursively(val, current.right);
+      }
+    }
+    return this;
   }
-
-  /** find(val): search the tree for a node with value val.
-   * return the node, if found; else undefined. Uses iteration. */
 
   find(val) {
-
+    let current = this.root;
+    while (current) {
+      if (val === current.val) return current;
+      current = val < current.val ? current.left : current.right;
+    }
+    return undefined;
   }
 
-  /** findRecursively(val): search the tree for a node with value val.
-   * return the node, if found; else undefined. Uses recursion. */
-
-  findRecursively(val) {
-
+  findRecursively(val, current = this.root) {
+    if (!current) return undefined;
+    if (val === current.val) return current;
+    return val < current.val
+      ? this.findRecursively(val, current.left)
+      : this.findRecursively(val, current.right);
   }
-
-  /** dfsPreOrder(): Traverse the array using pre-order DFS.
-   * Return an array of visited nodes. */
 
   dfsPreOrder() {
-
+    const visited = [];
+    function traverse(node) {
+      if (!node) return;
+      visited.push(node.val);
+      traverse(node.left);
+      traverse(node.right);
+    }
+    traverse(this.root);
+    return visited;
   }
-
-  /** dfsInOrder(): Traverse the array using in-order DFS.
-   * Return an array of visited nodes. */
 
   dfsInOrder() {
-
+    const visited = [];
+    function traverse(node) {
+      if (!node) return;
+      traverse(node.left);
+      visited.push(node.val);
+      traverse(node.right);
+    }
+    traverse(this.root);
+    return visited;
   }
-
-  /** dfsPostOrder(): Traverse the array using post-order DFS.
-   * Return an array of visited nodes. */
 
   dfsPostOrder() {
-
+    const visited = [];
+    function traverse(node) {
+      if (!node) return;
+      traverse(node.left);
+      traverse(node.right);
+      visited.push(node.val);
+    }
+    traverse(this.root);
+    return visited;
   }
-
-  /** bfs(): Traverse the array using BFS.
-   * Return an array of visited nodes. */
 
   bfs() {
-
-  }
-
-  /** Further Study!
-   * remove(val): Removes a node in the BST with the value val.
-   * Returns the removed node. */
-
-  remove(val) {
-
-  }
-
-  /** Further Study!
-   * isBalanced(): Returns true if the BST is balanced, false otherwise. */
-
-  isBalanced() {
-
-  }
-
-  /** Further Study!
-   * findSecondHighest(): Find the second highest value in the BST, if it exists.
-   * Otherwise return undefined. */
-
-  findSecondHighest() {
-    
+    const visited = [];
+    const queue = [this.root];
+    while (queue.length) {
+      const node = queue.shift();
+      if (node) {
+        visited.push(node.val);
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+      }
+    }
+    return visited;
   }
 }
 
